99ac314ccc303544630e16a222b6e9316ae6c57b
郭玉光 <guoyuguang@higinet.com.cn>
2019-01-23 10:25:39 +0800

    add icon branch

--- src/pages/home/components/Icon.vue
+++ src/pages/home/components/Icon.vue
@@ -1,11 +1,118 @@
 <template>
-  <div>icon</div>
+  <div class="icons">
+    <swiper :options="swiperOption" >
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.img" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      swiperOption: {
+      },
+      imgList: [
+        {
+          id: '001',
+          img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '热门景点'
+        },
+        {
+          id: '002',
+          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '热门景点'
+        },
+        {
+          id: '003',
+          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '热门景点'
+        }, {
+          id: '004',
+          img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '热门景点'
+        },
+        {
+          id: '005',
+          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '热门景点'
+        },
+        {
+          id: '006',
+          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '热门景点'
+        }, {
+          id: '007',
+          img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '热门景点'
+        },
+        {
+          id: '008',
+          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '热门景点'
+        },
+        {
+          id: '009',
+          img: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '热门景点'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.imgList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      console.log(pages)
+      return pages
+    }
+  }
 }
 </script>
 <style lang='stylus' scoped>
-
+@import '~styles/varibles.styl';
+.icons >>> .swiper-container
+  height 0
+  padding-bottom 50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
 </style>
